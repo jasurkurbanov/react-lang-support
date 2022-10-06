156af30018c7798eaf0f7b56c0986ca3d2af5161
Jasur Kurbanov <jasurkurbanov96@gmail.com>
2022-10-06 09:09:57 +0500

    feat: translation separated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const { t, i18n } = useTranslation()
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +11,26 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <select onChange={(e) => i18n.changeLanguage(e.target.value)}>
+          <option>Choose language</option>
+          <option value="uz">Uzbek</option>
+          <option value="ru">Russian</option>
+          <option value="en">English</option>
+        </select>
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {t('welcome')}
+          <br />
+          {t('goodbye')}
         </a>
+
+
       </header>
     </div>
   );
